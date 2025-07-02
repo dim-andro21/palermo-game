a327456119f844070b358e11a3ca0001398df4c1
dim-andro21 <androdimitris@gmail.com>
2025-07-02 16:19:59 +0300

    PWA

--- script.js
+++ script.js
@@ -988,3 +988,9 @@ function getRoleIcon(role) {
     };
     return map[role] || "❓";
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('✅ Service Worker Registered'))
+    .catch(err => console.error('❌ Service Worker registration failed:', err));
+}
